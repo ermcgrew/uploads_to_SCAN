f4e1705c8f4344bdd06fa8f0536f692edd383c1a
Emily McGrew <emcgrew@bscsub1.pmacs.upenn.edu>
2023-03-15 10:03:42 -0400

    config file; code as already developed

---
+++ find_sessions_create_csv.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import flywheel
+import csv
+from datetime import datetime
+from config import email,password
+
+
+def write_csv(data, scantype):
+    if scantype == "MRI":
+        headernames = mri_columns
+    elif scantype == "FBBPET":
+        headernames = pet_columms
+    time = datetime.now().strftime("%Y%m%d_%H%M")
+    with open(f"{scantype}_sessions_for_SCAN_{time}.csv", "w", newline="") as csvfile:
+        csvwriter = csv.DictWriter(csvfile, fieldnames=headernames)
+        csvwriter.writeheader()
+        csvwriter.writerows(data)
+    return
+
+
+def find_pet_times(acquisitions):
+    for acq in acquisitions:
+        if "BR-DY_CTAC" in acq.label and "LOCALIZER" not in acq.label:
+            acq = acq.reload()
+            dicom_file_index = [
+                x for x in range(0, len(acq.files)) if acq.files[x].type == "dicom"
+            ][0]
+            pet_times_list_temp = [acq.files[dicom_file_index].info["RadiopharmaceuticalInformationSequence"]["RadionuclideTotalDose"]]
+                ## conversion to from Becuerel to mCi
+            pet_times_list_temp.append("000000") ## fake data to hold place for Tracer Dose Time from dosage computer
+            pet_times_list_temp.append(
+                acq.files[dicom_file_index].info[
+                    "RadiopharmaceuticalInformationSequence"
+                ]["RadiopharmaceuticalStartTime"]
+            )
+            pet_times_list_temp.append(
+                acq.files[dicom_file_index].info["AcquisitionTime"]
+            )
+            result = list(map(format_times, pet_times_list_temp))
+            return result
+
+
+def format_times(time):
+    return datetime.strftime(
+        datetime.strptime(time.split(".")[0], "%H%M%S"), "%H:%M:%S"
+    )
+
+
+# Global variables
+subject_total = 0
+session_total = 0
+mri_list_to_write = []
+pet_list_to_write = []
+mri_columns = ["Subject ID", "Directory", "Sedation", "Eyes Open", "Notes"]
+pet_columms = [
+    "Subject ID",
+    "Directory",
+    "Scan Date",
+    "Tracer",
+    "Tracer Dose Assay",
+    "Tracer Dose Time",
+    "Tracer Inj Time",
+    "Emission Start Time",
+    "Comments",
+]
+
+# Access flywheel subject list
+fw = flywheel.Client()
+try:
+    project = fw.get_project("5c508d5fc2a4ad002d7628d8")  # NACC-SC
+except flywheel.ApiException as e:
+    print(f"Error: {e}")
+try:
+    subjects = project.subjects.iter_find("created>2022-06-01")  #'created>2022-06-01'
+except flywheel.ApiException as e:
+    print(f"Error: {e}")
+
+# for each subject, keep sessions only if they are ABC, at least one session 2021 or later (consent form changed), have 3T and FBBPET sessions
+# new criteria: only if 3T session has Accelerated Sagital MPRAGE file 
+for subject in subjects:
+    acq_test = [[acquisition.label for acquisition in session.acquisitions() if "Accelerated Sagittal MPRAGE (MSV21)" in acquisition.label] for session in subject.sessions() if "3T" in session.label and "ABC" in session.label]    
+    if acq_test and acq_test[0]:
+        print(f"Subject {subject.label} has AccSag file in 3T scan, checking for PET scans")
+        pet_test = [session.label for session in subject.sessions() if "FBBPET" in session.label or "AV1451" in session.label]
+        if pet_test:
+            print([session.label for session in subject.sessions() if "Duplicate" not in session.tags
+            and "Misc." not in session.tags
+            and "ABC" in session.label
+            and "7T" not in session.label]) #count of sessions to upload from
+
+########################################################################
+            for session in subject.sessions():
+                if "3T" in session.label:
+                    session_total += 1
+            #         print(f"This session should be added: {session.label}")
+
+            #         # download dicom
+            #         # save file loc
+
+            #         mri_data_list = [subject.label, "temp", "No"]
+            #         mri_list_to_write.append(dict(zip(mri_columns, mri_data_list)))
+
+            #         # acqs=[acquisition.label for acquisition in session.acquisitions()]
+            #         # if "Accelerated Sagittal MPRAGE (MSV21)" in acqs:
+            #         #     print(f"new acquisition type")
+
+            #     elif "FBBPET" in session.label: ######change to account for tau scans as well
+            #         session_total += 1
+            #         print(f"This session should be added: {session.label}")
+            #         # download dicom
+            #         # save file loc
+            #         pet_times_list = find_pet_times(session.acquisitions())
+
+            #         pet_data_list = [
+            #             subject.label,
+            #             "temp",
+            #             str(session.timestamp)[:10],
+            #             "Florbetaben",
+            #         ]
+            #         pet_data_list.extend(pet_times_list)
+            #         pet_list_to_write.append(dict(zip(pet_columms, pet_data_list)))
+            #     else:
+            #         continue
+    else:
+        continue
+
+# print(f"{subject_total} subjects have sessions to upload.")
+# print(f"{session_total} total sessions will be uploaded.")
+
+# write_csv(mri_list_to_write, "MRI")
+# write_csv(pet_list_to_write, "FBBPET")
+
+
+# call to java program
+# java -jar IdaUploader_02Dec2022.jar
+# --email=email --password=password
+# --project=SCAN --site=ADC21
+# SCAN_MRI_upload_test_20230203.csv
